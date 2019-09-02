68c8358bae92cd1f5efc344b0df8aacb91927c6b
Seba_virtual <rata_6952@hotmail.com>
2019-09-02 16:20:42 -0400

    se agregan cards y style

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_144756) do
+ActiveRecord::Schema.define(version: 2019_09_02_160855) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
